734e2e37a20f3d34e819b363c854d2348d901e57
GarrettBPowell <garettbpowell@gmail.com>
2021-03-16 16:13:59 -0500

    Start trying to make button go away while still allowing it to complete any current operations

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,7 +10,11 @@ public class GameManager : MonoBehaviour
     public int level = 1;
     public bool outOfCombat = false;
 
-   
+    //UI STUFF
+    public Button moveButton;
+    public Button attackButton;
+
+
     private void Awake()
     {
         GameObject[] gameManager = GameObject.FindGameObjectsWithTag("gameManager");
@@ -29,6 +34,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (outOfCombat)
+        {
+            moveButton.gameObject.GetComponent<Image>().enabled = false;
+            attackButton.gameObject.SetActive(false);
+        }
 
+        else
+        {
+            moveButton.gameObject.GetComponent<Image>().enabled = true;
+            attackButton.gameObject.SetActive(true);
+        }
     }
 }
