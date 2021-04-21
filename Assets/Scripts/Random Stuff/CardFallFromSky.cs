20da90975d9978534e34fa23b0fb8bb33999e045
Benjamin Blackmon <benmblackmon@gmail.com>
2021-04-20 19:18:05 -0500

    Imported turning animation for title screen for starter and cave cards

---
+++ Assets/Scripts/Random Stuff/CardFallFromSky.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFallFromSky : MonoBehaviour
+{
+    [SerializeField] float minY, maxY;
+    [SerializeField] float minX, maxX;
+    [SerializeField] float speed;
+
+    SpriteRenderer sr;
+    Transform tr;
+    Animator animator;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        tr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        SetToTop();
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(0, (-speed * Time.deltaTime), 0));
+
+        if(transform.position.y <= minY)
+        {
+            SetToTop();
+        }
+    }
+
+    private void SetToTop()
+    {
+        int rand = Random.Range(0, 8);
+
+        switch (rand)
+        {
+            case 0:
+                sr.color = new Color(1, 1, 1, 0.2f);
+                sr.sortingOrder = 0;
+                tr.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                animator.speed = 1;
+                speed = 5;
+                break;
+            case 1:
+                sr.color = new Color(1, 1, 1, 0.3f);
+                sr.sortingOrder = 1;
+                tr.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+                animator.speed = 0.9f;
+                speed = 4.5f;
+                break;
+            case 2:
+                sr.color = new Color(1, 1, 1, 0.4f);
+                sr.sortingOrder = 2;
+                tr.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                animator.speed = 0.8f;
+                speed = 4;
+                break;
+            case 3:
+                sr.color = new Color(1, 1, 1, 0.5f);
+                sr.sortingOrder = 3;
+                tr.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                animator.speed = 0.7f;
+                speed = 3.5f;
+                break;
+            case 4:
+                sr.color = new Color(1, 1, 1, 0.6f);
+                sr.sortingOrder = 4;
+                tr.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+                animator.speed = 0.6f;
+                speed = 3;
+                break;
+            case 5:
+                sr.color = new Color(1, 1, 1, 0.7f);
+                sr.sortingOrder = 5;
+                tr.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                animator.speed = 0.5f;
+                speed = 2.5f;
+                break;
+            case 6:
+                sr.color = new Color(1, 1, 1, 0.8f);
+                sr.sortingOrder = 6;
+                tr.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                animator.speed = 0.4f;
+                speed = 2;
+                break;
+            case 7:
+                sr.color = new Color(1, 1, 1, 0.9f);
+                sr.sortingOrder = 7;
+                tr.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+                animator.speed = 0.3f;
+                speed = 1.5f;
+                break;
+            case 8:
+                sr.color = new Color(1, 1, 1, 1);
+                sr.sortingOrder = 8;
+                tr.localScale = new Vector3(1, 1, 1);
+                animator.speed = 0.2f;
+                speed = 1;
+                break;
+            default:
+                break;
+        }
+        tr.position = new Vector3(Random.Range(minX, maxX), maxY, 0);
+    }
+
+
+}
