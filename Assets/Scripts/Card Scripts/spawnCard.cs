40a32ec3138b3f3f781a4cd130ea8fff86f53c2e
GarrettBPowell <garettbpowell@gmail.com>
2021-04-24 14:43:29 -0500

    enemies drop cards yaya

--- Assets/Scripts/Card Scripts/spawnCard.cs
+++ Assets/Scripts/Card Scripts/spawnCard.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class spawnCard : MonoBehaviour
 {
     public int chance;
-    public List<GameObject> cardsToSpawnFrom;
+    public List<GameObject> caveCards;
+    public List<GameObject> mountainCards;
+    public List<GameObject> forestCards;
     private void OnDestroy()
     {
         int randNum = Random.Range(0, 100);
         if(randNum < chance)
         {
-            Instantiate(cardsToSpawnFrom[Random.Range(0, cardsToSpawnFrom.Count)], gameObject.transform.position, Quaternion.identity);
+            if(gameObject.GetComponent<Enemy>().enemyZone.Equals("cave"))
+                Instantiate(caveCards[Random.Range(0, caveCards.Count)], gameObject.transform.position, Quaternion.identity);
+
+            else if (gameObject.GetComponent<Enemy>().enemyZone.Equals("mountain"))
+                Instantiate(mountainCards[Random.Range(0, mountainCards.Count)], gameObject.transform.position, Quaternion.identity);
+
+            else if (gameObject.GetComponent<Enemy>().enemyZone.Equals("forest"))
+                Instantiate(forestCards[Random.Range(0, forestCards.Count)], gameObject.transform.position, Quaternion.identity);
         }
     }
 }
