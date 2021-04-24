de796b11bb3099aaff2b46bff4e983778c52f86f
GarrettBPowell <garettbpowell@gmail.com>
2021-04-24 15:46:53 -0500

    card text update, camera color update

---
+++ Assets/Scripts/Random Stuff/changeCameraColor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeCameraColor : MonoBehaviour
+{
+    public List<Color> cameraColor;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Level Screen"))
+        {
+            if(gameManager.area == 2)
+            {
+                if(gameManager.level % 2 == 1)
+                {
+                    gameObject.GetComponent<Camera>().backgroundColor = cameraColor[1];
+                }
+                else
+                    gameObject.GetComponent<Camera>().backgroundColor = cameraColor[2];
+            }
+        }
+    }
+}
