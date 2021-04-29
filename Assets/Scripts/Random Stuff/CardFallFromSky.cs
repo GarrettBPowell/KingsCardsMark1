54e832aa2a113759005b6b9fc93851cb470aa45c
Benjamin Blackmon <benmblackmon@gmail.com>
2021-04-29 17:43:30 -0500

    sgersergserg

--- Assets/Scripts/Random Stuff/CardFallFromSky.cs
+++ Assets/Scripts/Random Stuff/CardFallFromSky.cs
@@ -7,6 +7,7 @@ public class CardFallFromSky : MonoBehaviour
     [SerializeField] float minY, maxY;
     [SerializeField] float minX, maxX;
     [SerializeField] float speed;
+    [SerializeField] float opacity;
 
     SpriteRenderer sr;
     Transform tr;
@@ -37,63 +38,63 @@ public class CardFallFromSky : MonoBehaviour
         switch (rand)
         {
             case 0:
-                sr.color = new Color(1, 1, 1, 0.2f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 0;
                 tr.localScale = new Vector3(0.2f, 0.2f, 0.2f);
                 animator.speed = 1;
                 speed = 5;
                 break;
             case 1:
-                sr.color = new Color(1, 1, 1, 0.3f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 1;
                 tr.localScale = new Vector3(0.3f, 0.3f, 0.3f);
                 animator.speed = 0.9f;
                 speed = 4.5f;
                 break;
             case 2:
-                sr.color = new Color(1, 1, 1, 0.4f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 2;
                 tr.localScale = new Vector3(0.4f, 0.4f, 0.4f);
                 animator.speed = 0.8f;
                 speed = 4;
                 break;
             case 3:
-                sr.color = new Color(1, 1, 1, 0.5f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 3;
                 tr.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 animator.speed = 0.7f;
                 speed = 3.5f;
                 break;
             case 4:
-                sr.color = new Color(1, 1, 1, 0.6f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 4;
                 tr.localScale = new Vector3(0.6f, 0.6f, 0.6f);
                 animator.speed = 0.6f;
                 speed = 3;
                 break;
             case 5:
-                sr.color = new Color(1, 1, 1, 0.7f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 5;
                 tr.localScale = new Vector3(0.7f, 0.7f, 0.7f);
                 animator.speed = 0.5f;
                 speed = 2.5f;
                 break;
             case 6:
-                sr.color = new Color(1, 1, 1, 0.8f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 6;
                 tr.localScale = new Vector3(0.8f, 0.8f, 0.8f);
                 animator.speed = 0.4f;
                 speed = 2;
                 break;
             case 7:
-                sr.color = new Color(1, 1, 1, 0.9f);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 7;
                 tr.localScale = new Vector3(0.9f, 0.9f, 0.9f);
                 animator.speed = 0.3f;
                 speed = 1.5f;
                 break;
             case 8:
-                sr.color = new Color(1, 1, 1, 1);
+                sr.color = new Color(opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x, opacity * tr.localScale.x);
                 sr.sortingOrder = 8;
                 tr.localScale = new Vector3(1, 1, 1);
                 animator.speed = 0.2f;
